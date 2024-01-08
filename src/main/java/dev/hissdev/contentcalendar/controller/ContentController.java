3ce5c924987cca5d3f0668c3ae9aec31696f237d
HissDev <hissoffc@gmail.com>
2024-01-08 18:59:41 +0300

    Создал контроллер и метод которые выводят все записи из репозитория

--- src/main/java/dev/hissdev/contentcalendar/controller/ContentController.java
+++ src/main/java/dev/hissdev/contentcalendar/controller/ContentController.java
@@ -1,13 +1,26 @@
 package dev.hissdev.contentcalendar.controller;
 
+import dev.hissdev.contentcalendar.model.Content;
+import dev.hissdev.contentcalendar.repository.ContentCollectionRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 // путь до контроллера
 @RequestMapping("/api/content")
 public class ContentController {
 
+    private final ContentCollectionRepository repository;
 
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
 
 }
--- src/main/java/dev/hissdev/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/dev/hissdev/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,8 +1,12 @@
 package dev.hissdev.contentcalendar.repository;
 
 import dev.hissdev.contentcalendar.model.Content;
+import dev.hissdev.contentcalendar.model.Status;
+import dev.hissdev.contentcalendar.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -25,6 +29,21 @@ public class ContentCollectionRepository {
     public Optional<Content> findById(Integer id) {
 //        Поиск по id и возврат первого совпадения
         return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+//    Инициализаця для отправки данных по api
+    @PostConstruct
+    public void init() {
+        Content c = new Content(1,
+                "My firs post in a blog",
+                "Hello, this is my first post!",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                ""
+                );
+        contentList.add(c);
     }
 
 }
