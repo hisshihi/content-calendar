01256fcaf5cccd494168b279dc8cc37708567445
HissDev <hissoffc@gmail.com>
2024-01-08 16:10:19 +0300

    Создал пакет для контроллеров и сам контроллер

---
+++ src/main/java/dev/hissdev/contentcalendar/controller/ContentController.java
@@ -0,0 +1,13 @@
+package dev.hissdev.contentcalendar.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// путь до контроллера
+@RequestMapping("/api/content")
+public class ContentController {
+
+    
+
+}
