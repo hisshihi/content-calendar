35c8630f4abf6d6d82d42deb976af2538839c5a6
HissDev <hissoffc@gmail.com>
2024-01-08 14:40:48 +0300

    Инициализация проекта Spring boot

---
+++ src/main/java/dev/hissdev/contentcalendar/ContentCalendarApplication.java
@@ -0,0 +1,15 @@
+package dev.hissdev.contentcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalendarApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ContentCalendarApplication.class, args);
+
+	}
+
+}
