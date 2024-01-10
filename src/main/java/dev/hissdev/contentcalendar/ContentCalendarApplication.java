a371b80a0d9da54a0992b8aa3759b74d5d6eeeeb
HissDev <hissoffc@gmail.com>
2024-01-10 16:24:33 +0300

    Создал несколько метаданных. Также создал контроллер для вывода этих данных и конструктор, для создания данных

--- src/main/java/dev/hissdev/contentcalendar/ContentCalendarApplication.java
+++ src/main/java/dev/hissdev/contentcalendar/ContentCalendarApplication.java
@@ -1,5 +1,6 @@
 package dev.hissdev.contentcalendar;
 
+import dev.hissdev.contentcalendar.config.ContentCalendarProperties;
 import dev.hissdev.contentcalendar.model.Content;
 import dev.hissdev.contentcalendar.model.Status;
 import dev.hissdev.contentcalendar.model.Type;
@@ -7,10 +8,12 @@ import dev.hissdev.contentcalendar.repository.ContentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
 
+@EnableConfigurationProperties(ContentCalendarProperties.class)
 @SpringBootApplication
 public class ContentCalendarApplication {
 
